ecca9e3e16746f325baa497bab3badf4d238857c
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-03-25 22:00:48 +0300

    fix: unexport the id of a tag

--- business/core/crud/medicine/tag.go
+++ business/core/crud/medicine/tag.go
@@ -4,11 +4,16 @@ import "github.com/google/uuid"
 
 // Tag represents a single tag.
 type Tag struct {
-	ID		uuid.UUID
+	id		uuid.UUID
 	name 	string
 }
 
 // Name returns the name of a tag.
 func (t Tag) Name() string {
 	return t.name
+}
+
+// Id returns the id of a tag.
+func (t Tag) Id() uuid.UUID {
+	return t.id
 }
