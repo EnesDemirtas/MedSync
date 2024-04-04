48b71a8ff4fc79681e1e8604e794a2c5761f0743
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-04-05 00:21:29 +0300

    feat: implement user in app layer

---
+++ app/core/crud/userapp/userapp.go
@@ -0,0 +1,164 @@
+// Package userapp maintains the app layer api for the user domain.
+package userapp
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/mail"
+	"time"
+
+	"github.com/EnesDemirtas/medisync/app/api/errs"
+	"github.com/EnesDemirtas/medisync/app/api/mid"
+	"github.com/EnesDemirtas/medisync/app/api/page"
+	"github.com/EnesDemirtas/medisync/business/api/auth"
+	"github.com/EnesDemirtas/medisync/business/core/crud/userbus"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// Core manages the set of app layer api functions for the user domain.
+type Core struct {
+	userBus *userbus.Core
+	auth    *auth.Auth
+}
+
+// NewCore constructs a user core API for use.
+func NewCore(userBus *userbus.Core, auth *auth.Auth) *Core {
+	return &Core{
+		userBus: userBus,
+		auth:	 auth,
+	}
+}
+
+// Token provides an API token for the authenticated user.
+func (c *Core) Token(ctx context.Context, kid string, addr mail.Address, password string) (Token, error) {
+	usr, err := c.userBus.Authenticate(ctx, addr, password)
+	if err != nil {
+		switch {
+		case errors.Is(err, userbus.ErrNotFound):
+			return Token{}, errs.New(errs.FailedPrecondition, err)
+		case errors.Is(err, userbus.ErrAuthenticationFailure):
+			return Token{}, errs.New(errs.Unauthenticated, err)
+		default:
+			return Token{}, fmt.Errorf("authenticate: %w", err)
+		}
+	}
+
+	claims := auth.Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   usr.ID.String(),
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: usr.Roles,
+	}
+
+	tkn, err := c.auth.GenerateToken(kid, claims)
+	if err != nil {
+		return Token{}, errs.New(errs.Internal, err)
+	}
+
+	return toToken(tkn), nil
+}
+
+// Create adds a new user to the system.
+func (c *Core) Create(ctx context.Context, app NewUser) (User, error) {
+	nc, err := toBusNewUser(app)
+	if err != nil {
+		return User{}, errs.New(errs.FailedPrecondition, err)
+	}
+
+	usr, err := c.userBus.Create(ctx, nc)
+	if err != nil {
+		if errors.Is(err, userbus.ErrUniqueEmail) {
+			return User{}, errs.New(errs.Aborted, userbus.ErrUniqueEmail)
+		}
+		return User{}, errs.Newf(errs.Internal, "create: usr[%+v]: %s", usr, err)
+	}
+
+	return toAppUser(usr), nil
+}
+
+// Update updates an existing user.
+func (c *Core) Update(ctx context.Context, app UpdateUser) (User, error) {
+	uu, err := toBusUpdateUser(app)
+	if err != nil {
+		return User{}, errs.New(errs.FailedPrecondition, err)
+	}
+
+	usr, err := mid.GetUser(ctx)
+	if err != nil {
+		return User{}, errs.Newf(errs.Internal, "user missing in context: %s", err)
+	}
+
+	updUsr, err := c.userBus.Update(ctx, usr, uu)
+	if err != nil {
+		return User{}, errs.Newf(errs.Internal, "update: userID[%s] uu[%+v]: %s", usr.ID, uu, err)
+	}
+
+	return toAppUser(updUsr), nil
+}
+
+// UpdateRole updates an existing user's role.
+func (c *Core) UpdateRole(ctx context.Context, app UpdateUserRole) (User, error) {
+	uu, err := toBusUpdateUserRole(app)
+	if err != nil {
+		return User{}, errs.New(errs.FailedPrecondition, err)
+	}
+
+	usr, err := mid.GetUser(ctx)
+	if err != nil {
+		return User{}, errs.Newf(errs.Internal, "user missing in context: %s", err)
+	}
+
+	updUsr, err := c.userBus.Update(ctx, usr, uu)
+	if err != nil {
+		return User{}, errs.Newf(errs.Internal, "updaterole: userID[%s] uu[%+v]: %s", usr.ID, uu, err)
+	}
+
+	return toAppUser(updUsr), nil
+}
+
+// Delete removes a user from the system.
+func (c *Core) Delete(ctx context.Context) error {
+	usr, err := mid.GetUser(ctx)
+	if err != nil {
+		return errs.Newf(errs.Internal, "userID missing in context: %s", err)
+	}
+
+	if err := c.userBus.Delete(ctx, usr); err != nil {
+		return errs.Newf(errs.Internal, "delete: userID[%s]: %s", usr.ID, err)
+	}
+
+	return nil
+}
+
+// Query returns a list of users with paging.
+func (c *Core) Query(ctx context.Context, qp QueryParams) (page.Document[User], error) {
+	if err := validatePaging(qp); err != nil {
+		return page.Document[User]{}, err
+	}
+
+	filter, err := parseFilter(qp)
+	if err != nil {
+		return page.Document[User]{}, err
+	}
+
+	orderBy, err := parseOrder(qp)
+	if err != nil {
+		return page.Document[User]{}, err
+	}
+
+	usrs, err := c.userBus.Query(ctx, filter, orderBy, qp.Page, qp.Rows)
+	if err != nil {
+		return page.Document[User]{}, errs.Newf(errs.Internal, "query: %s", err)
+	}
+
+	total, err := c.userBus.Count(ctx, filter)
+	if err != nil {
+		return page.Document[User]{}, errs.Newf(errs.Internal, "count: %s", err)
+	}
+
+	return page.NewDocument(toAppUsers(usrs), total, qp.Page, qp.Rows), nil
+}
